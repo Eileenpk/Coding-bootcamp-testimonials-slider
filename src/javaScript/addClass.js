2831b6367cb7a097a339f0d2d72eec70168a5f63
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-01-28 12:05:25 -0500

    feat: setup unit testing

---
+++ src/javaScript/addClass.js
@@ -0,0 +1,5 @@
+ function addClass (element, classToAdd) {
+    element.classList.add(classToAdd)
+}
+
+module.exports = addClass
--- test.js
+++ test.js
@@ -1,1 +1,23 @@
-import assert from 'assert';
+const assert = require("assert");
+const expect = require('expect.js')
+const { it, describe } = require("mocha");
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const addClass = require('./src/javaScript/addClass')
+
+describe("addClass", () => {
+    let dom;
+beforeEach(() => {
+    dom = new JSDOM(`<!DOCTYPE html>`);
+    global.document = dom.window.document;
+});
+    it("adds a class to an element", () => {
+        const element = document.createElement('div');
+        const input = 'translate'
+        addClass(element, input);
+
+        expect(element.classList.contains('translate')).to.be.true
+        // assert.deepEqual(element.classList.add, input);
+    });
+});
